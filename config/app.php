21d7e0710016118f7b48eb5e547fcee19033830a
vanni2249 <vanni2249@gmail.com>
2022-12-13 13:06:31 -0600

    add package laravel breeze

--- config/app.php
+++ config/app.php
@@ -56,6 +56,10 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
+
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
+
     'asset_url' => env('ASSET_URL'),
 
     /*
@@ -69,7 +73,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Puerto_Rico',
 
     /*
     |--------------------------------------------------------------------------
